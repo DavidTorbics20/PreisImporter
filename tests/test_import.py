88f435c52d394e5eed2269ed5aee6b0e9ba53592
DavidTorbics20 <torbic190220@sr.htlweiz.at>
2024-01-09 12:49:19 +0100

    reader -> checking for valid separators

--- tests/test_import.py
+++ tests/test_import.py
@@ -25,3 +25,30 @@ def test_file_not_exists():
         reader = Reader(wrong_file)
         assert reader
 
+
+def test_file_content():
+    reader = Reader(FILEPATH)
+    df = reader.readFile()
+    assert df != ''
+
+    dummy_data = "Firma, Epoch, Wert, Währung, Börse;" \
+                 "Lenzing, 170447112, 34.75, EUR, Vienna;" \
+                 "Andritz, 170447131, 59.41, USD, New York;" \
+                 "EVN, 170447132, 28.55, EUR, Vienna;" \
+                 "EVN, 170447133, 31.18, USD, New York;"
+
+    valid_df = pd.read_csv(dummy_data)
+
+    assert df == valid_df
+
+
+def test_data_separators():
+    reader = Reader(FILEPATH)
+    df = reader.readFile()
+    assert df != ''
+
+    rows = df.split(';')
+    columns = rows[0].split(',')
+
+    assert len(rows) == 5
+    assert len(columns) == 5
